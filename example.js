0e4df4cb33368e739c6832d42c9bb83acbbd024f
weberchang <weber87na@gmail.com>
2026-09-07 15:07:58 +0800

    更新 readme

--- example.js
+++
@@ -1,5 +0,0 @@
-function capture() {
-    const v = document.querySelector("video"), c = document.querySelector("#canvas");
-    c.width = v.videoWidth; c.height = v.videoHeight;
-    c.getContext("2d").drawImage(v, 0, 0, c.width, c.height);
-}
